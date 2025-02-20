d3669958a352b7084d8349204d8f3c852aed7ea9
Sephrodite <132352821+Sephrodite@users.noreply.github.com>
2025-02-20 14:12:04 +0200

    added dice rolling
    dice rolling mechanic added

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ load_dotenv()
 import os
 import discord
 from discord.ext import commands
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,6 +18,22 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
 	await ctx.send(f'Hello, {ctx.author.display_name}!')
+	
+@bot.command()
+async def roll(ctx, dice: str):
+    try: 
+        notation = dice.lower()
+        parts = notation.split('d')
+        dice_number = int(parts[0])
+        dice_type = int(parts[1])
+    except Exception as e:
+        await ctx.send(f'please enter a valid number')
+        return
+    total = 0
+    for _ in range(dice_number):
+        roll = random.randint(1, dice_type)
+        total += roll
+    await ctx.send(f'You rolled a total of {dice_number} dice, the result per dice should be between 1 and {dice_type}. The total (if more than 1) is: {total}')
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
