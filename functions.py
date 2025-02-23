915ecdc5778dfdf5a685f7ad9017c13680156b2b
Sephrodite <132352821+Sephrodite@users.noreply.github.com>
2025-02-23 18:32:23 +0200

    added skill rolling function
    added a function to roll a skill

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import random
 import json
+import math
 
 CHAR_FILE = "characters.json"
 
@@ -20,3 +21,16 @@ def save_characters(characters):
             json.dump(characters, file, indent=4)
     except FileNotFoundError:
         return {}
+
+def roll_default(char_skill):
+    roll = (random.randint(1, 100))
+    if roll <= char_skill:
+        roll= str(roll)
+        char_skill = str(char_skill)
+        msg = "You rolled a " +  roll + " against " + char_skill + "! Check successfull"
+        return msg
+    else:
+        roll= str(roll)
+        char_skill = str(char_skill)
+        msg = "You rolled a " +  roll + " against " + char_skill + "! Check failed"
+        return msg
