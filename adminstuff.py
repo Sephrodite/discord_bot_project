03f9e2632746cb85d30f0789e7c2ef6ec846eb92
Sephrodite <132352821+Sephrodite@users.noreply.github.com>
2026-05-09 11:14:19 +0300

    must be a string
    let's try this again but with a string

--- adminstuff.py
+++ adminstuff.py
@@ -20,5 +20,6 @@ def fixpoints(char_name, points):
             if character_name == char_name:
                 characters[user_id][character_name]["points"]=int(points) + int(characters[user_id][character_name]["points"])
                 save_characters(characters)
+                points = str(points)
                 msg = "Points for " + char_name + " have been set to " + points + "."
                 return msg
